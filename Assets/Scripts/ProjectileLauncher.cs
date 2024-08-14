847e9d015a51c7c3cb58b87b6c997722e35dd74b
Hafidz Maulana <hafidz.maulana465@gmail.com>
2024-08-14 23:39:34 +0700

    Fixing Arrow Bug

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -15,8 +15,18 @@ public class ProjectileLauncher : MonoBehaviour
         // Flip the projectile's facing direction and movement based on the direction the character is facing at time of lunch
         projectile.transform.localScale = new Vector3(
             origScale.x * transform.localScale.x > 0 ? 1 : -1,
-            origScale.y, 
+            origScale.y,
             origScale.z
         );
+
+        bool isFacingRight = transform.localScale.x > 0;
+    
+        // Set the projectile's rotation based on the direction
+        projectile.transform.eulerAngles = new Vector3(
+            0, 
+            0, 
+            isFacingRight ? 225 : 135 // 0 degrees for right, 180 degrees for left
+    );
     }
 }
+
